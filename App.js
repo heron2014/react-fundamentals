3e6fc2530347ff9bb33e55e7f9a0026cb2694721
Anita <a.nita@hotmail.co.uk>
2016-04-16 20:11:58 +0100

    manage the state see https://github.com/heron2014/react-fundamentals/issues/7#issuecomment-210875596

--- App.js
+++ App.js
@@ -6,19 +6,18 @@ class App extends React.Component {
     super();
     this.state = {txt: 'this is a state value'}
   }
+
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
   render() {
-    return <h1>{this.state.txt}</h1>
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
   }
 }
-
-// App.propTypes = {
-//   txt: React.PropTypes.string,
-//   cat: React.PropTypes.number.isReguired
-// }
-//
-// App.defaultProps = {
-//   txt: 'this is a default value'
-// }
-// export default App
 
 ReactDOM.render(<App />, document.getElementById('app'));
