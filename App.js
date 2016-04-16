d8a15c5807ecff48e4c6a8a94b56eb9f9dafdd9c
Anita <a.nita@hotmail.co.uk>
2016-04-16 19:18:32 +0100

    props see #6

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <div>Hola!</div>
+    let txt = this.props.txt;
+    return <h1>{txt}</h1>
   }
 }
 
-export default App
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isReguired
+}
+
+App.defaultProps = {
+  txt: 'this is a default value'
+}
+// export default App
+
+ReactDOM.render(<App cat={5} />, document.getElementById('app'));
